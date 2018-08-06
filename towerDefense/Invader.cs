f585b1c7d8d8218ad6568c190bd10a96d31b2d59
Epineda312 <epineda773@protonmail.com>
2018-08-06 00:50:59 -0700

    Update Invader.cs
    Added some getters and setters for the Invaders MapLocation Property

--- towerDefense/Invader.cs
+++ towerDefense/Invader.cs
@@ -2,6 +2,17 @@ namespace treehouseDefense
 {
   class Invader
   {
-    
-  }
+     private MapLocation _location;
+                
+    public MapLocation Location
+    {
+      get
+      {
+        return _location;
+      }
+      set
+      {
+        _location = value;
+      }
+    }     
 }
