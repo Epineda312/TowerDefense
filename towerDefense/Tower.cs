190d98eb204f3953d9abf804632d0042c3f69c03
Epineda312 <epineda773@protonmail.com>
2018-08-13 00:19:58 -0700

    Update Tower.cs

--- towerDefense/Tower.cs
+++ towerDefense/Tower.cs
@@ -1,7 +1,29 @@
-namespace treehouseDefense
+namespace TreehouseDefense
 {
   class Tower 
   {
+    private const int _range = 1;
+    private const int _power = 1;
     
+    private readonly MapLocation _location;
+    
+    public Tower(MapLocation location)
+    {
+      _location = location;
+    }
+    
+    public void FireOnInvaders(Invader[] invaders)
+    {             
+      foreach(Invader invader in invaders)
+      {
+        //do stuff with invader
+        if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
+        {
+          invader.DecreaseHealth(_power);
+          break;
+        }
+      }                  
+    }          
   }
 }
+
