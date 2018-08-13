c2999e43dfefb2f2514ae00f00b50a634986b929
Epineda312 <epineda773@protonmail.com>
2018-08-12 23:17:22 -0700

    Update Invader.cs

--- towerDefense/Invader.cs
+++ towerDefense/Invader.cs
@@ -10,6 +10,15 @@ namespace TreehouseDefense
     
     public int Health{ get; private set; } = 2;
     
+    //True if the invader has reached the end of the path
+    public bool HasScored {get { return _pathStep >= _path.Length; } }
+    
+    //True if invader has no health left
+    public bool IsNeutralized => Health <= 0;
+    
+    //True is the invader has health and has not scored yet
+    public bool IsActive => !(IsNeutralized || HasScored);
+    
     public Invader(Path path)
     {
         _path = path;
