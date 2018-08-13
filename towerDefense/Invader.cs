3b1ac143759587ef21bccf22208cd21ffb9ec335
Epineda312 <epineda773@protonmail.com>
2018-08-12 22:58:23 -0700

    Update Invader.cs

--- towerDefense/Invader.cs
+++ towerDefense/Invader.cs
@@ -1,18 +1,25 @@
-namespace treehouseDefense
+namespace TreehouseDefense
 {
   class Invader
   {
-     private MapLocation _location;
-                
-    public MapLocation Location
+    
+    private readonly Path _path;    
+    private int _pathStep = 0;
+        
+    public MapLocation Location => _path.GetLocationAt(_pathStep); 
+    
+    public int Health{ get; private set; } = 2;
+    
+    public Invader(Path path)
     {
-      get
-      {
-        return _location;
-      }
-      set
-      {
-        _location = value;
-      }
-    }     
+        _path = path;
+    }
+    
+    public void Move() => _pathStep += 1;   
+    
+    public void DecreaseHealth(int factor)
+    {
+      Health -= factor;
+    }
+  }    
 }
