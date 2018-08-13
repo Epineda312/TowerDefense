1eb126edc010e0bc3d181a1d47153243b61f73b5
Epineda312 <epineda773@protonmail.com>
2018-08-13 00:28:00 -0700

    Add files via upload

---
+++ towerDefense/Level.cs
@@ -0,0 +1,20 @@
+namespace TreehouseDefense
+{
+    class Level
+    {
+      private readonly Invader[] _invaders;
+      public Level(Invader[] invaders)
+      {
+        _invaders = invaders;
+      }
+      
+      public bool Play()
+      {
+        //Run untill all invaders are neutralized or an invader reaches the end of the path
+        
+        //Each tower has opportunity to fire on invaders.
+        
+        //Count and move the invaders that are still active
+      }
+    }
+}
