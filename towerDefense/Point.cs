6b2067fa532075bc00ac1cba5c628e2a752bc2ba
Epineda312 <epineda773@protonmail.com>
2018-08-17 19:11:52 -0700

    Update Point.cs

--- towerDefense/Point.cs
+++ towerDefense/Point.cs
@@ -1,30 +1,26 @@
 using System;
 
-namespace TreehouseDefense
+namespace TowerDefense
 {
     class Point
-  {
-    public readonly int X;
-    public readonly int Y;
-      
-    public Point(int x, int y)
     {
-      X = x;
-      Y = y;
-    }  
-    
-    public int DistanceTo(int x, int y)
-    {
-        int xDiff = X - x;
-        int yDiff = Y - y;
-      
-        int xDiffSquared = xDiff * xDiff;
-        int yDiffSquared = yDiff * yDiff;
-      
-        return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
-      
-      return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
+        public readonly int X;
+        public readonly int Y;
         
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public int DistanceTo(int x, int y)
+        {
+            return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
+        }
+        
+        public int DistanceTo(Point point)
+        {
+            return DistanceTo(point.X, point.Y);
+        }
     }
- } 
 }
