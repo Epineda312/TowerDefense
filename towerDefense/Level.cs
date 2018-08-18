6af6f1f283ac94aaf2d63bab62154f08ce5e6d88
Epineda312 <epineda773@protonmail.com>
2018-08-17 19:10:37 -0700

    Update Level.cs

--- towerDefense/Level.cs
+++ towerDefense/Level.cs
@@ -1,20 +1,49 @@
-namespace TreehouseDefense
+namespace TowerDefense
 {
     class Level
     {
-      private readonly Invader[] _invaders;
-      public Level(Invader[] invaders)
-      {
-        _invaders = invaders;
-      }
-      
-      public bool Play()
-      {
-        //Run untill all invaders are neutralized or an invader reaches the end of the path
+        private readonly Invader[] _invaders;
         
-        //Each tower has opportunity to fire on invaders.
+        public Tower[] Towers { get; set; }
         
-        //Count and move the invaders that are still active
-      }
+        public Level(Invader[] invaders)
+        {
+            _invaders = invaders;
+        }
+        
+        // Returns: true if the player wins, false otherwise.
+        public bool Play()
+        {
+            // Run until all invaders are neutralized or an invader reaches the end of the path.
+            int remainingInvaders = _invaders.Length;
+            
+            while(remainingInvaders > 0)
+            {
+                // Each tower has opportunity to fire on invaders.
+                foreach(Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
+                
+                // Count and move the invaders that are still active.
+                remainingInvaders = 0;
+                foreach(Invader invader in _invaders)
+                {
+                    if(invader.IsActive)
+                    {
+                        invader.Move();
+                        
+                        if(invader.HasScored)
+                        {
+                            return false;
+                        }
+                        
+                        remainingInvaders++;
+                    }
+                }
+            }
+            
+            return true;
+        }
     }
 }
