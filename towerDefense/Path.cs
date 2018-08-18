8714fd9e1c7ee194b04a90a939210859ce7c4333
Epineda312 <epineda773@protonmail.com>
2018-08-17 19:11:29 -0700

    Update Path.cs

--- towerDefense/Path.cs
+++ towerDefense/Path.cs
@@ -1,17 +1,19 @@
-namespace TreehouseDefense
+namespace TowerDefense
 {
-  class Path
-  {
-      private readonly MapLocation[] _path;  
-    
-      public Path(MapLocation[] path)
-      {
-          _path = path;
-      }
-    
-    public MapLocation GetLocationAt(int pathStep)
+    class Path
     {
-      return (pathStep < _path.Length) ? _path[pathStep] : null;            
+        private readonly MapLocation[] _path;
+        
+        public int Length => _path.Length;
+        
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+        
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
-  }
 }
